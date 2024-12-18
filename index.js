359593c8c42a0378a8aa985ea76473dd84b771b1
KeiRey <reyyankeimal1@gmail.com>
2024-12-18 22:13:13 +0700

    [update]

--- index.js
+++ index.js
@@ -2,17 +2,25 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = 8000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.get('/about', (req, res) => {
+    res.send('About route 🎉 ')
+})
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const uploadDir = path.join(__dirname, 'uploads');
-    cb(null, uploadDir); 
-  },
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    cb(null, uniqueSuffix + '-' + file.originalname); 
-  }
+    destination: (req, file, cb) => {
+        const uploadDir = path.join(__dirname, 'uploads');
+        cb(null, uploadDir);
+    },
+    filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, uniqueSuffix + '-' + file.originalname);
+    }
 });
 
 const upload = multer({ storage: storage });
@@ -20,19 +28,19 @@ const upload = multer({ storage: storage });
 app.use(express.json());
 
 app.post('/upload', upload.single('file'), (req, res) => {
-  try {
-    if (!req.file) {
-      return res.status(400).send({ message: 'No file uploaded.' });
+    try {
+        if (!req.file) {
+            return res.status(400).send({ message: 'No file uploaded.' });
+        }
+        res.status(200).send({
+            message: 'File uploaded successfully!',
+            file: req.file
+        });
+    } catch (error) {
+        res.status(500).send({ message: 'File upload failed.', error });
     }
-    res.status(200).send({
-      message: 'File uploaded successfully!',
-      file: req.file
-    });
-  } catch (error) {
-    res.status(500).send({ message: 'File upload failed.', error });
-  }
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
